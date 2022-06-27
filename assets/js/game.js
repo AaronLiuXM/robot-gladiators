df9467443651d109a56ad02c1d3e7f0d9370894e
Aaron Liu <aaron155658@gmail.com>
2022-06-27 15:39:03 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,10 @@
 // Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//    * Fight all enemy-robots
-//    * Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
 
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+  };
 
 // this creates a function named "fight"
 var playerName = window.prompt("What is your robot's name?");
@@ -11,11 +12,14 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+//function to generate a random numeric value
+
+
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber();
 var enemyAttack = 12;
 
 console.log(enemyNames);
@@ -43,7 +47,9 @@ var fight = function(enemyName) {
             }
         }
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-            enemyHealth = enemyHealth - playerAttack;
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+
+            enemyHealth = Math.max(0,enemyHealth - damage);
             console.log(
                 playerName + " attacked " + enemyName + ". " + " now has " + enemyHealth + " health remaining."
             );
@@ -57,7 +63,9 @@ var fight = function(enemyName) {
             }
 
             // remove player's health by subtrating the amount set in the enemyAttack variable
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+            playerHealth = Math.max(0,playerHealth - damage);
             console.log(
                 enemyName + " attacked " + playerName + ". " + " now has " + playerHealth+ " health remaining."
             );
@@ -86,7 +94,7 @@ var startGame = function() {
     
             var pickedEnemyName = enemyNames[i];
     
-            enemyHealth = 50;
+            enemyHealth = randomNumber();
     
             fight(pickedEnemyName);
 
@@ -139,33 +147,41 @@ var shop = function() {
 
     // use switch to carry out action
     switch (shopOptionPrompt) {
+        case "REFILL": // new case
         case "refill":
-            if (playerMoney >= 7) {
-                window.alert("Refilling player's health by 20 for 7 dollars.");
-
-                // increase health and decrease money
-                playerHealth = playerHealth + 20;
-                playerMoney = playerMoney - 7;
-            }
-            else {
-                window.alert("You don't have enough money!");
-            }
-
-            break;
-            case "upgrade":
-            if (playerMoney >= 7) {
-                window.alert("Upgrading player's attack by 6 for 7 dollars.");
-
-            // increase attack and decrease money
-                playerAttack = playerAttack + 6;
-                playerMoney = playerMoney - 7;
-            }
-            else {
-                window.alert("You don't have enough money!");
-            }
-
-            break;
-    }
+          if (playerMoney >= 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars.");
+      
+            playerHealth = playerHealth + 20;
+            playerMoney = Math.max(0,playerMoney - 10);
+          }
+          else {
+            window.alert("You don't have enough money!");
+          }
+      
+          break;
+        case "UPGRADE": // new case
+        case "upgrade":
+          if (playerMoney >= 7) {
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+      
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+          }
+          else {
+            window.alert("You don't have enough money!");
+          }
+      
+          break;
+        case "LEAVE": // new case
+        case "leave":
+          window.alert("Leaving the store.");
+          break;
+        default:
+          window.alert("You did not pick a valid option. Try again.");
+          shop();
+          break;
+      }
   };
 
 
